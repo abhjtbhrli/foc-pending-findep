ee5a579bf19b31e8ccd85fbc5d367bf4f0880070
Abhijit Bharali <37649445+abhjtbhrli@users.noreply.github.com>
2026-04-20 13:08:25 +0530

    Add multiple lists for various departments and categories

--- excl.py
+++ excl.py
@@ -2,3 +2,19 @@ EXCL = [
   'FIN/DIS/SCE/001/2026/1274',
   'FIN/DIS/SCE/001/2026/1275'
 ]
+
+
+PWD = ['Public Works (Buildings & NH) Department', 'Public Works (Roads) Department', 'Public Works (Health and Education) Department']
+
+
+
+SENIORMOST_list = ['Mr.Dilip Kumar BorahIAS,Secretary', 'Mr.JayantNarlikar , IAS ,Commissioner & Secretary']
+
+SOPD_list = ['SOPD-FDR', 'SOPD-G', 'SOPD-ODS', 'SOPD-SCSP', 'SOPD-TSP', 'TG-AC', 'TG-DC', 'TG-EI', 'TG-IB', 'TG-SFC', 'TG-SSA', 'TG-UL']
+RIDF_list = ['RIDF-LS', 'RIDF-SS', 'WIF-LS', 'WIF-SS']
+TG_list = ['TG-FFC', 'TG-CFC']
+CSS_list = ['CSS', 'SOPD-SS', 'EE-CS', 'EE-SS']
+EAP_list = ['EAP', 'EAP-SS']
+NIDA_list = ['NIDA-LS', 'NIDA-SS']
+UIDF_list = ['UIDF-LS', 'UIDF-SS']
+SCDF_list = ['SCDF-LS', 'SCDF-SS']
